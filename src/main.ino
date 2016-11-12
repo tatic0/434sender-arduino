87cf4e054028080d1cf3d111df3658c1905050d8
F Varas <tatico@gmail.com>
2016-11-12 18:34:05 +0100

    ...off and on again

--- src/main.ino
+++ src/main.ino
@@ -6,12 +6,17 @@
 */
 
 #include <RCSwitch.h>
-
 RCSwitch mySwitch = RCSwitch();
+
+long AON = 4212049;
+long AOFF = 4212052;
+long BON = 4215121;
+long BOFF =4215124;
 
 void setup() {
 
   Serial.begin(9600);
+  Serial.println("Setup...");
   
   // Transmitter is connected to Arduino Pin #10  
   mySwitch.enableTransmit(10);
@@ -31,14 +36,18 @@ void loop() {
 
 
   /* Same switch as above, but using decimal code */
-  mySwitch.send(4212049, 24);
+  mySwitch.send(AON, 24);
+	Serial.println("AON");
   delay(5000);  
-  mySwitch.send(4212052, 24);
+  mySwitch.send(AOFF, 24);
+	Serial.println("AOFF");
   delay(5000);  
-  mySwitch.send(4215121, 24);
+  mySwitch.send(BON, 24);
+	Serial.println("BON");
   delay(5000);  
-  mySwitch.send(4215124, 24);
-  delay(5000);  
+  mySwitch.send(BOFF, 24);
+	Serial.println("BOFF");
+  delay(5000);
 
 
 //  delay(20000);
